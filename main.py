abb701765225145ca9e49cacdad4f17e254bf2b4
Anton Angeletti <angelettianton@gmail.com>
2022-10-09 19:28:01 -0500

    rework --skip-download stuff, also add some error handling

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import pytube
 from pytube import YouTube
 from argparse import ArgumentParser
 import os
+import errno
 
 class SheetGrabber:
-    def __init__(self, link):
+    # link: the link to the youtube video to grab sheet music from
+    # extension: the file extension to download and save the video as
+    def __init__(self, link, extension = 'mp4'):
         self.link = link
+        self.extension = extension
 
         # this is a dumb way to check if the link is valid but I couldn't think of a better one
         # initially set that the link is invalid, if it gets through try/except, set it to True
@@ -26,23 +30,42 @@ class SheetGrabber:
             # print(f'ERROR: video unavailable, error was {e.__class__.__name__}')
         else:
             self.valid_link = True
-            print(f'Found "{self.video.title}"')
+            print(f'Found video "{self.video.title}"')
 
     # download the video at self.link, saving it to filename
-    # by default, filename is set to the video's title
-    # skip parameter is for skipping the pytube download and looking for the video in current directory
-    def download(self, filename = None, skip = False):
-        self.filename = filename if filename else self.video.title
-        if skip:
-            if not os.path.exists(self.filename + '.mp4'):
-                # TODO: error handling is all over the place, gotta fix that
-                raise ValueError
-            print(f'Found {self.filename}.mp4')
-        else:
-            print('downloading the video...')
-            # download the video into current directory, filtering for video only (no audio)
-            self.video.streams.filter(only_video=True, file_extension='mp4', adaptive=True).first().download(filename=self.filename + '.mp4')
-            print(f'Done downloading, saved to {self.filename}.mp4')
+    def download(self, filename):
+        print('Downloading the video off youtube... (might take a sec)')
+        # search for a video stream to download, filtering for video only (no audio)
+        stream = self.video.streams.filter(only_video=True, file_extension=self.extension, adaptive=True).first()
+        # TODO: if filepath exists, don't download, instead throw an error recommending --skip-download
+        filepath = f'{filename}.{self.extension}'
+        # download the video
+        # TODO: some kind of progress bar/output, this takes a while and prints nothing
+        stream.download(filename=filepath)
+        print(f'Done downloading, saved to {filepath}')
+        self.filename = filename
+        self.filepath = filepath
+
+    # skip downloading the video, just use a video file instead
+    # this mainly exists to set self.filename and verify the video file exists
+    def skip_download(self, filename):
+        filepath = f'{filename}.{self.extension}'
+        print(f'Skipping video download, looking for {filepath}...')
+        if not os.path.isfile(filepath):
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filepath)
+        print(f'Found {filepath}')
+        self.filename = filename
+        self.filepath = filepath
+
+    # finds the default OS-compatible filename provided by pytube for the downloaded video
+    # useful for when the user doesn't specify filename and you need to find one to use
+    # returns just the stem of the filename, doesn't include file extension
+    def find_filename(self):
+        # this returns a default filename including the file extension
+        filepath = self.video.streams.first().default_filename
+        # strip the extension, then return
+        filename = os.path.splitext(filepath)[0]
+        return filename
 
 
 def main():
@@ -55,7 +78,12 @@ def main():
     grabber = SheetGrabber(args.link)
     if not grabber.valid_link:
         return
-    grabber.download(args.filename, args.skip_download)
+
+    filename = args.filename if args.filename else grabber.find_filename()
+    if args.skip_download:
+        grabber.skip_download(filename)
+    else:
+        grabber.download(filename)
 
 if __name__ == '__main__':
     main()
